de6e9761f35b46f667b07c0b97152a155962a88e
catster <mirololeo@gmail.com>
2026-09-21 22:05:58 +0100

    Prototyping Development v3

--- Prototyping/Scenes/TestingScene/path_drawer.gd
+++ Prototyping/Scenes/TestingScene/path_drawer.gd
@@ -7,6 +7,7 @@ func _process(_delta: float) -> void:
 
 func _draw() -> void:
 	for i in range(1, gamescene.drawn_path.size()):
+		var hero_col = gamescene.HeroRules[gamescene.current_hero_path]["color"]
 		var pos = gamescene.tilemap_ground.map_to_local(gamescene.drawn_path[i])
 		var prev_pos = gamescene.tilemap_ground.map_to_local(gamescene.drawn_path[i - 1])
-		draw_line(prev_pos, pos, Color("006fffff"), 10.0, false)
+		draw_line(prev_pos, pos, hero_col, 10.0, false)
--- Prototyping/Scenes/TestingScene/testing_scene.gd
+++ Prototyping/Scenes/TestingScene/testing_scene.gd
@@ -2,9 +2,9 @@ extends Node2D
 class_name GameTest
 
 enum HEROS { WARRIOR, LUMBERJACK, ARCHER, MINER }
-var HeroRules = {
-	HEROS.WARRIOR: { "color": Color("6babff"), "name": "Warrior" },
-	HEROS.LUMBERJACK: { "color": Color("ff6b8e"), "name": "Lumberjack" },
+@onready var HeroRules = {
+	HEROS.WARRIOR: { "color": Color("6babff"), "name": "Warrior", "ref": $Warrior },
+	HEROS.LUMBERJACK: { "color": Color("ff6b8e"), "name": "Lumberjack", "ref": $Lumberjack },
 	HEROS.ARCHER: { "color": Color("75ff6b"), "name": "Archer" },
 	HEROS.MINER: { "color": Color("ffba6b"), "name": "Miner" },
 }
@@ -12,21 +12,26 @@ var HeroRules = {
 @onready var gamecamera: Camera2D = $Camera2D
 
 @onready var tilemap_ground: TileMapLayer = $TMGrass
+@onready var tilemap_trees: TileMapLayer = $TMTrees
 @onready var tilemap_highlight: TileMapLayer = $TMHighlight
 var tilemap_highlight_atlas: int
 var current_hover_position := Vector2i(-100, -100)
 
 var full_path_queue = []
+var full_path_queue_chars = []
 var run_mode_active: bool = false
 @onready var char_warrior = $Warrior
 
 var drawn_path := []
 var drawing_path_active: bool = false
+var current_hero_path: HEROS = HEROS.WARRIOR
 @onready var confirm_path_button: Button = $"../Interface/UI/ConfirmPath"
 @onready var start_path_button: Button = $"../Interface/UI/Start"
+@onready var retry_path_button: Button = $"../Interface/UI/ResetPath"
 
 func _ready() -> void:
 	tilemap_highlight_atlas = tilemap_highlight.tile_set.get_source_id(0)
+	$"../Interface/UI/HeroMode".color = HeroRules[current_hero_path]["color"]
 
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
@@ -52,7 +57,7 @@ func _input(event: InputEvent) -> void:
 	
 	if event is InputEventMouseButton:
 		# Awful bad stupid code
-		if control_node_clicked(confirm_path_button, event):
+		if control_node_clicked(confirm_path_button, event) or control_node_clicked(retry_path_button, event):
 			return
 		if control_node_clicked(start_path_button, event):
 			return
@@ -77,6 +82,17 @@ func _input(event: InputEvent) -> void:
 			gamecamera.zoom += Vector2(0.015, 0.015)
 			gamecamera.zoom.x = max(0.45, gamecamera.zoom.x)
 			gamecamera.zoom.y = max(0.45, gamecamera.zoom.y)
+	
+	if event is InputEventKey:
+		if event.is_action_released("char_1"):
+			print("Character Lane 1")
+			drawn_path = []
+			current_hero_path = HEROS.WARRIOR
+		elif event.is_action_released("char_2"):
+			print("Character Lane 2")
+			drawn_path = []
+			current_hero_path = HEROS.LUMBERJACK
+		$"../Interface/UI/HeroMode".color = HeroRules[current_hero_path]["color"]
 
 func update_hover_position() -> void:
 	tilemap_highlight.clear()
@@ -85,48 +101,77 @@ func update_hover_position() -> void:
 		tilemap_highlight.set_cell(current_hover_position, tilemap_highlight_atlas, Vector2i(0, 0))
 
 func add_cell_to_path(cell_position: Vector2i) -> void:
-	if tilemap_ground.get_cell_source_id(cell_position) == -1:
-		# Not on the tilemap path
+	if not hero_can_enter(cell_position):
 		return
 	
 	if drawn_path.is_empty():
 		drawn_path.append(cell_position)
+		queue_redraw()
 		return
 	
-	# Undoing while drawing
-	var last_cell = drawn_path[-1]
-	if cell_position == last_cell: return
+	var last_cell: Vector2i = drawn_path[-1]
+
+	if cell_position == last_cell:
+		return
 	
+	# Undoing while drawing backwards
 	if len(drawn_path) >= 2 and cell_position == drawn_path[-2]:
 		drawn_path.pop_back()
 		queue_redraw()
 		return
 	
-	# Fill in cells skipped during diagonal
-	var diff: Vector2i = cell_position - last_cell
+	var diff := cell_position - last_cell
 	
-	if diff.y == 0:
-		var step = sign(diff.x)
-		for x in range(last_cell.x + step, cell_position.x + step, step):
-			drawn_path.append(Vector2i(x, last_cell.y))
+	# Only support horizontal/vertical movement
+	if diff.x != 0 and diff.y != 0:
+		return
 	
-	if diff.x == 0:
-		var step = sign(diff.y)
-		for y in range(last_cell.y + step, cell_position.y + step, step):
-			drawn_path.append(Vector2i(last_cell.x, y))
+	var step: Vector2i
 	
+	if diff.x != 0:
+		step = Vector2i(sign(diff.x), 0)
+	else:
+		step = Vector2i(0, sign(diff.y))
+	
+	var cells_to_add: Array[Vector2i] = []
+	var check_cell := last_cell + step
+	
+	while check_cell != cell_position + step:
+		if not hero_can_enter(check_cell):
+			return
+		
+		cells_to_add.append(check_cell)
+		check_cell += step
+	
+	drawn_path.append_array(cells_to_add)
 	queue_redraw()
+
+func is_specified_tilemap(tm: TileMapLayer, cell: Vector2i) -> bool:
+	return tm.get_cell_source_id(cell) != -1
+
+func is_walkable_tile(cell: Vector2i) -> bool:
+	return is_specified_tilemap(tilemap_ground, cell)
+
+func hero_can_enter(cell: Vector2i) -> bool:
+	if is_specified_tilemap(tilemap_ground, cell):
+		return true
+	
+	if is_specified_tilemap(tilemap_trees, cell):
+		return (current_hero_path == HEROS.LUMBERJACK)
+	
+	return false
 
 var queue_item_scene = load("res://Prototyping/Scenes/TestingScene/Queue/Queue.tscn")
 
 func on_confirm_path_pressed() -> void:
 	confirm_path_button.visible = false
 	full_path_queue.append(drawn_path)
+	full_path_queue_chars.append(current_hero_path)
 	
 	var qi = queue_item_scene.instantiate()
 	$"../Interface/UI/QueuePanel/ScrollContainer/Queue".add_child(qi)
 	qi.path = drawn_path
-	qi.init(self, HEROS.WARRIOR, len(drawn_path))
+	qi.init(self, current_hero_path, len(drawn_path))
 	
 	drawn_path = []
 
@@ -143,12 +188,34 @@ func start_pressed() -> void:
 	confirm_path_button.visible = false
 	run_simulation()
 
+func reset_path_pressed() -> void:
+	get_tree().reload_current_scene()
+
 func run_simulation() -> void:
-	char_warrior.position = full_path_queue[0][0]
-	for path in full_path_queue:
+	await get_tree().create_timer(0.1).timeout
+	
+	for idx in range(0, len(full_path_queue)):
+		var path = full_path_queue[idx]
+		var hero = full_path_queue_chars[idx]
+		var heroref = HeroRules[hero]["ref"]
+		
 		for next_position in path: 
 			var tween = create_tween()
 			var pos = tilemap_ground.map_to_local(next_position)
-			await tween.tween_property(char_warrior, "position", pos, 0.3).finished
+			await tween.tween_property(heroref, "position", pos, 0.1).finished
+			
+			if is_specified_tilemap(tilemap_trees, next_position):
+				if hero == HEROS.LUMBERJACK:
+					await destroy_obstacle(tilemap_trees, next_position)
+			
 			await get_tree().create_timer(0.2).timeout
 		await get_tree().create_timer(0.65).timeout
+	
+	await get_tree().create_timer(0.4).timeout
+	retry_path_button.visible = true
+
+func destroy_obstacle(obstacle_tm: TileMapLayer, cell: Vector2i) -> void:
+	await get_tree().create_timer(2).timeout
+	obstacle_tm.erase_cell(cell)
+	tilemap_ground.set_cell(cell, 1, Vector2i(0,0))
+	return
